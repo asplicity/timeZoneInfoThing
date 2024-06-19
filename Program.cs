16f0d51d4a2c02e5333c22525428691d290e2ece
asplicity <ticobeier@gmail.com>
2024-06-19 14:56:06 +0200

    q

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nenne eine Zeitzone");
+
+        var Timezone = Console.ReadLine() ?? string.Empty;
+        var Now = DateTime.Now;
+
+        var newTime = TimeZoneInfo.ConvertTime(Now, TimeZoneInfo.FindSystemTimeZoneById(Timezone));
+        Console.WriteLine($"Wenn es hier {Now} ist, ist es in {Timezone} {newTime}, {Timezone} geht {(Now-newTime).TotalHours} Stunden nach");
+    }
+}
